c090492708b11377336ffa9ab46848c421f60fb7
Jesús Peiró López <jpeirolo@mercadona.es>
2024-02-07 13:21:49 +0100

    Initial solution twoSum with tests

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -5,11 +5,58 @@ package org.tfoc;
  */
 public class Solution {
 
-    public int[] twoSum(
-            int[] nums,
-            int target) {
+    //minimum and maximum values "nums[i]" and "target" can have
+    private static final int MIN_VALIDITY = -109;
+    private static final int MAX_VALIDITY = 1_000_000_000;
 
-        return null;
+    //maximum length nums.length can have
+    private static final int MAX_LENGTH = 10_000;
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        if(isInvalid(target)){
+            System.out.println("Invalid target number");
+            return new int[0];
+        }
+
+        if(nums.length<2||nums.length>MAX_LENGTH){
+            System.out.println("Invalid nums length");
+            return new int[0];
+        }
+
+        if(isInvalid(nums[0])){
+            System.out.println("Invalid value on nums");
+            return new int[0];
+        }
+
+
+        boolean solutionFound = false;
+        int[] solution = new int[0];
+
+        //Loop through all values
+        for(int i = 0; i< nums.length; i++){
+            for(int j = 1; j< nums.length; j++){
+                if(isInvalid(nums[j]) && i==0){
+                    System.out.println("Invalid value on nums");
+                    return new int[0];
+                }
+                //If no solution was found yet, solution equals both numbers
+                if((nums[i]+nums[j])==target && !solutionFound){
+                    solution = new int[]{i, j};
+                    solutionFound = true;
+                  //If solution was found again, then empty array is returned
+                } else if((nums[i]+nums[j])==target){
+                    System.out.println("More than one solution");
+                    return new int[0];
+                }
+            }
+        }
+
+        return solution;
+    }
+
+    public static boolean isInvalid(int value) {
+        return value < MIN_VALIDITY || value > MAX_VALIDITY;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -3,13 +3,96 @@ package org.tfoc;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 @Slf4j
 class SolutionTest {
 
     @Test
-    void testSolution() {
+    void successfulSolution() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[]{0,1}, result);
+
     }
+
+    @Test
+    void emptySolution() {
+
+        int[] nums = new int[]{};
+        int target = 9;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[0], result);
+
+    }
+
+    @Test
+    void onlyOneValueOnNumsSolution() {
+
+        int[] nums = new int[]{2};
+        int target = 9;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[0], result);
+
+    }
+
+    @Test
+    void invalidTargetNumberSolution() {
+
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = -1223;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[0], result);
+
+    }
+
+    @Test
+    void invalidFirstNumberOnNumsSolution() {
+
+        int[] nums = new int[]{-1122, 7, 11, 15};
+        int target = 9;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[0], result);
+
+    }
+
+    @Test
+    void invalidNumberOnNumsSolution() {
+
+        int[] nums = new int[]{2, 7, -2323, 15};
+        int target = 9;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[0], result);
+
+    }
+
+    @Test
+    void moreThanOneValidSolution() {
+
+        int[] nums = new int[]{3, 7, 4, 6};
+        int target = 10;
+
+        var result = Solution.twoSum(nums, target);
+
+        assertArrayEquals(new int[0], result);
+
+    }
+
+
+
 
 }
